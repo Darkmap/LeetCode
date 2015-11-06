75376a99298ddbc032c6fc0f2eaff2fcd1b00c6c
qixuanwang <darkmap0209@gmail.com>
2015-11-06 13:49:54 -0800

    211 commit

---
+++ src/Leetcode211.java
@@ -0,0 +1,20 @@
+/**
+ * Created by qixuanwang on 15/11/6.
+ */
+public class Leetcode211 {
+
+
+    public static void main(String[] args) {
+
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("word");
+        System.out.println(wordDictionary.search("..d"));
+    }
+
+
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
+}
